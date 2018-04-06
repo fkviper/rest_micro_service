85911e717b11ed66c233e0425e38d34a277fddb2
Harajyoti Das <HarajyotiD@talentica-all.com>
2018-04-06 12:23:22 +0530

    removed commented code

--- rest_service_using_beast/include/config.h
+++ rest_service_using_beast/include/config.h
@@ -12,15 +12,6 @@ public:
 	/// Defaults to 1 thread.
 	std::size_t thread_pool_size = 1;
 
-	/// Timeout on request handling. Defaults to 5 seconds.
-	//long timeout_request = 5;
-
-	/// Timeout on content handling. Defaults to 300 seconds.
-	//long timeout_content = 300;
-
-	/// Maximum size of request stream buffer. Defaults to architecture maximum.
-	/// Reaching this limit will result in a message_size error code.
-	//std::size_t max_request_streambuf_size = std::numeric_limits<std::size_t>::max();
 	/// IPv4 address in dotted decimal form or IPv6 address in hexadecimal notation.
 	/// If empty, the address will be any address.
 	std::string address_;
